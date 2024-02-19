be3b2d1b733b5901688c8d7d1437b554fad9e028
roliroli76 <dominik.rolek@gmail.com>
2024-02-19 18:46:46 +0100

    szablony

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog_app import views
 
 app_name = 'blog'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail')
+    path('blog/', include('blog_app.urls', namespace='blog_app'))
+
 ]
